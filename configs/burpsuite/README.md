9322d780ddf541cad5e1bb2a41cc25b09edba54a
1trapbox <118535944+1trapbox@users.noreply.github.com>
2023-12-13 04:01:29 +0800

    update

---
+++ configs/burpsuite/README.md
@@ -0,0 +1,4 @@
+## 打包exe
+- [PS2EXE 仓库地址](https://github.com/MScholtes/PS2EXE)
+版本号还挺好用的
+![Alt text](image.png)
---
+++ configs/burpsuite/start_burp.ps1
@@ -0,0 +1,18 @@
+# ps1脚本 用于启动burpsuite
+
+# Set the working directory
+$workingDirectory = "E:\1trapshell\Burpsuite"
+Set-Location -Path $workingDirectory
+
+    pwsh -NoExit -WindowStyle Hidden -c {
+    # Run Java command
+    java `
+    -XX:+IgnoreUnrecognizedVMOptions `
+    --add-opens=java.desktop/javax.swing=ALL-UNNAMED `
+    --add-opens=java.base/java.lang=ALL-UNNAMED `
+    --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED `
+    --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED `
+    --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED `
+    -javaagent:burpsuitloader-3.7.17-all.jar=loader,hanizfy `
+    -jar burpsuite_pro_v2023.10.1.2.jar
+}
