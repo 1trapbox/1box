09a82e518a2eed09965c251b7b43c0050952116e
1trapbox <118535944+1trapbox@users.noreply.github.com>
2023-12-17 21:31:45 +0800

    🐛 FIX:

--- bash_script/auto_zsh_shell.sh
+++ bash_script/auto_zsh_shell.sh
@@ -100,7 +100,7 @@ function re_zshrc() {
 function install_zinit() {
     print_echo "正在将zsh设置为默认shell..."
     sleep 2
-    if sudo chsh -s "$(which zsh)"; then
+    if chsh -s "$(which zsh)"; then
         print_ok "设置zsh为默认shell 成功"
     else
         print_error "设置zsh为默认shell 失败"
